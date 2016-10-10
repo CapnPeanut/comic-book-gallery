1d0ef782a7d576fa62c66c0bae05625ca976ba08
Brian Atchley <capnpeanut@gmail.com>
2016-10-10 18:33:04 -0400

    Added Comic Book Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,13 +10,9 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
 
-            return Content("Hello from the comic books controller!");
-            
+
+            return View();
         }
     }
 }
